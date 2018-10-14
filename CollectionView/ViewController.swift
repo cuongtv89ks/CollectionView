8f3e98dd2d5ce6fa6e1bcc30c19d5d7e18ef5c8b
Tran Viet Cuong <vietcuong89ks@gmail.com>
2018-10-14 16:25:10 +0900

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Olive Union on 14/10/2018.
+//  Copyright © 2018 Olive Union. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
