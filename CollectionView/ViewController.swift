1b2d8437fbd209a9e28daa76b12449daa2fbc7ce
Tran Viet Cuong <vietcuong89ks@gmail.com>
2018-10-14 17:12:13 +0900

    get started

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -9,20 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var collectionView : UICollectionView!
+    
+    var CollectionData = ["1 🏆", "2 🐸", "3 🍩", "4 😸", "5 🤡", "6 👾", "7 👻", "8 🍖",
+                          "9 🎸", "10 🐯", "11 🐷", "12 🌋"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let width = (view.frame.size.width - 20 ) / 3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
     }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return CollectionData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = CollectionData[indexPath.row]
+        }
+        
+        return cell
     }
     
     
